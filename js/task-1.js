271ea389bec15ac0f7604429f1d3de5a9126a8e4
Normal One <149543061+Fedir-M@users.noreply.github.com>
2023-12-24 12:29:11 -0500

    homework04

--- js/task-1.js
+++ js/task-1.js
@@ -1,25 +1,69 @@
-console.log("Задача 1. Генератор slug")
+console.log("Задача 1. Пакування товарів")
 console.log("===========================")
 
-function  slugify(title) {
-    const normalezidTitle = title.toLowerCase();
-    const slug = normalezidTitle.replaceAll(" ", "-")
+//TODO Напиши функцію isEnoughCapacity(products, containerSize), 
+//TODO яка обчислює, чи помістяться всі товари в контейнер при пакуванні.
 
-    return slug;
+//TODO Функція оголошує два параметри:
+
+//TODO products — об’єкт, у якому ключі містять назви товарів, а їхні значення — кількість цих товарів. 
+//TODO Наприклад, { apples: 2, grapes: 4 }.
+//TODO containerSize — число, максимальна кількість одиниць товарів, яку в себе може вмістити контейнер.
+
+//TODO Функція має повернути результат перевірки, чи помістяться всі товари в контейнер. 
+//TODO Тобто порахувати загальну кількість товарів в об’єкті products і повернути true, 
+//TODO якщо вона менше або дорівнює containerSize, і false, якщо ні.
+
+function isEnoughCapacity(products, containerSize) {
+    //! ========================================== variant 1 (doesn't work)
+//     let totalQnty = 0;
+
+//     for (const product in products) {
+//         totalQnty += products[product];
+
+//         if(totalQnty <= containerSize){
+//             return true;
+//       }
+//       return false;
+//     }
+// }
+// ========================================== variant 2 (work)
+let totalQnty = 0;
+
+    for (const product in products) {
+        totalQnty += products[product];
+    }
+
+    if(totalQnty <= containerSize){
+        return true;
+  }
+  return false;
 }
-// =================== or like that=====================
-// function slugify(title) {
-//     const normalezidTitle = title.toLowerCase().split(" ");
-//     const slug = normalezidTitle.join("-");
-//     return slug;
-//   }    
-    
-  
+
+// ========================================== variant 3 (work)
+// let totalQnty = 0;
+// for (const product in products) {
+//     totalQnty += products[product];
+//   }
+
+//   return totalQnty <= containerSize ? true : false;
+// }
 
 // Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
 // У консоль будуть виведені результати її роботи.
 
-// "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(
+    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  ); // false
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+  ); // true
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+  ); // false
